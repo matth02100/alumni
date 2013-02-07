2cc882281a892b5499078a61ebdb550bca8a0b2b
matthieu <matthieu@matthieu>
2013-02-07 02:21:16 +0100

    premier commit pour mettre la lib et les premiers fichiers

---
+++ application/Bootstrap.php
@@ -0,0 +1,40 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+	public function run()
+	{
+		parent::run();
+	}
+	
+	protected function _initDoctype()
+	{
+		$this->bootstrap('view');
+		$view = $this->getResource('view');
+		$view->doctype('HTML5');
+	}
+	
+	protected function _initEncoding()
+	{
+		mb_internal_encoding("UTF-8");
+	}
+	
+	protected function _initConfig()
+	{
+		Zend_Registry::set('configs', new Zend_Config($this->getOptions()));
+	}
+	
+	protected function _initSession()
+	{
+		$session = new Zend_Session_Namespace('alumni', true);
+		return $session;
+	}
+	
+	protected function _initDbAdapter()
+	{
+		$db = Zend_Db::factory(Zend_Registry::get('configs')->database);
+		Zend_Db_Table_Abstract::setDefaultAdapter($db);
+		Zend_Registry::set('db', $db);
+	}
+}
+
